352662082b420b67be5f0bdb7ed1a8ed189a0dce
Kris Kowal <kris.kowal@cixar.com>
2014-06-05 18:35:34 -0700

    Manual deployment

---
+++ script/index.js
@@ -0,0 +1,6 @@
+var repl = require("./repl");
+
+var repls = document.querySelectorAll(".repl");
+for (var i = 0; i < repls.length; i++) {
+    repl(repls[i]);
+}
---
+++ script/repl.js
@@ -0,0 +1,84 @@
+var globalEval = eval;
+
+window.Dict = require("collections/dict");
+
+module.exports = function (element) {
+    evalSample(element);
+    addInput(element);
+};
+
+function evalSample(element) {
+    var statementEls = element.querySelectorAll(".repl-input");
+
+    for (var i = 0; i < statementEls.length; i++) {
+        var statementEl = statementEls[i];
+        var output = createOutputElement(evaluate(statementEl.textContent));
+
+        if (output) {
+            if (statementEl.nextElementSibling) {
+                element.insertBefore(output, statementEl.nextElementSibling);
+            } else {
+                element.appendChild(output);
+            }
+        }
+    }
+}
+
+function evaluate(source) {
+    var result;
+    try {
+        result = globalEval(source);
+        try {
+            result = JSON.stringify(result);
+        } catch (error) {
+            result = "<Unable to stringify " + result.toString() + ">";
+        }
+    } catch (error) {
+        result = error.name + ": " + error.message;
+    }
+    return result;
+}
+
+function createInputElement(source) {
+    var input = document.createElement("span");
+    input.classList.add("repl-input");
+    input.textContent = source + "\n";
+
+    return input;
+}
+
+function createOutputElement(result) {
+    if (typeof result !== "undefined") {
+        var output = document.createElement("span");
+        output.classList.add("repl-output");
+        output.textContent = result + "\n";
+
+        var comment = document.createElement("span");
+        comment.classList.add("repl-output-comment");
+        comment.textContent = "// ";
+        output.insertBefore(comment, output.lastChild);
+
+        return output;
+    }
+}
+
+function addInput(element) {
+    var container = document.createElement("div");
+    container.classList.add("repl-command");
+
+    var input = document.createElement("input");
+    input.placeholder = "\u276F";
+
+    input.addEventListener("keyup", function (event) {
+        if (event.keyCode === 13 && this.value) {
+            var input = createInputElement(this.value);
+            element.insertBefore(input, container);
+            var output = createOutputElement(evaluate(this.value));
+            element.insertBefore(output, container);
+            this.value = "";
+        }
+    });
+    container.appendChild(input);
+
+    element.appendChild(container);
+}
