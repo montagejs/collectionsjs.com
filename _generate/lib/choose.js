df1a9b4524fe0ed540b43eb460da87407ae612c5
Kris Kowal <kris.kowal@cixar.com>
2014-06-07 13:51:19 -0700

    Finish collection chooser

--- _generate/lib/choose.js
+++ _generate/lib/choose.js
@@ -1,10 +1,8 @@
 
-// WIP
-
 exports.Query = Query;
 function Query() {
-    this.shape = null; // "non-map", "map", "iterator"
-    this.keys = null; // "strings", "objects", "numbers", null
+    this.shape = null; // "flat", "map", "iterator"
+    this.keys = null; // "strings", "objects", "numbers", "any"
     this.sorted = false;
     this.unique = false; // not taken into consideration for maps
     this.capacity = false;
@@ -13,67 +11,186 @@ function Query() {
     this.multi = false;
     // range access
     this.first = false;
-    this.slice = false;
+    this.index = false;
     this.last = false;
     // range mutation
     this.shift = false;
-    this.swap = false;
+    this.splice = false;
     this.pop = false;
     this.long = false;
-    this.evil = null; // "gc" or "scan"
+    this.churn = null; // "gc" or "scan"
 }
 
-exports.answers = {
+exports.questions = {
 
     // Whether to ask
     "shape": function (_) {
-        return true;
-    },
-    "unique": function (_) {
-        return _.shape === "non-map";
-    },
-    "capacity": function (_) {
-        return _.shape === "map" || (_.shape === "non-map" && !_.unique);
-    },
-    "strategy": function (_) {
-        return _.capacity;
+        return "show";
     },
     "keys": function (_) {
-        return _.shape === "map" && !_.multi;
+        return (
+            _.shape === "map" && !_.multi && !_.capacity
+        ) ? "show" : "hide"
     },
-    "sorted": function (_) {
-        return !_.multi && (
-            (_.shape === "map") ||
-            (_.shape === "non-map")
-        );
+    "capacity": function (_) {
+        return (
+            !_.sorted &&
+            !_.multi &&
+            (
+                (_.shape === "map" && !_.weak) ||
+                (_.shape === "flat" && _.unique)
+            )
+        ) ? "show" : "hide";
+    },
+    "strategy": function (_) {
+        return (
+            this.capacity(_) && _.capacity
+        ) ? "show" : "hide";
+    },
+    "unique": function (_) {
+        return (
+            _.shape === "flat"
+        ) ? "show" : "hide";
+    },
+    "weak": function (_) {
+        return (
+            _.shape === "map" && _.keys === "objects"
+        ) ? "show" : "hide";
     },
     "ordered": function (_) {
+        return !_.capacity && (
+            (_.shape === "map" && _.keys !== "strings" && !_.multi && !_.weak) ||
+            (_.shape === "flat" && !_.multi && _.unique)
+        ) ? "show" : "hide";
+    },
+    "sorted": function (_) {
         return (
-            (_.shape === "map" && _.keys !== "strings" && !_.multi) ||
-            (_.shape === "non-map" && !_.multi && _.unique)
-        );
+            _.ordered && !_.multi && !_.capacity && (
+                (_.shape === "map") ||
+                (_.shape === "flat")
+            )
+        ) ? "show" : "hide"
+    },
+    "multi": function (_) {
+        return (
+            _.shape === "map" &&
+            !_.capacity &&
+            !_.sorted &&
+            !_.weak
+        ) ? "show" : "hide";
+    },
+    "long": function (_) {
+        return (
+            (
+                _.shape === "map" &&
+                !_.capacity &&
+                !_.multi &&
+                _.sorted
+            ) ||
+            (
+                _.shape === "flat" &&
+                (
+                    (
+                        _.unique &&
+                        _.sorted
+                    ) ||
+                    (
+                        !_.unique &&
+                        !_.sorted
+                    )
+                )
+            )
+        ) ? "show" : "hide";
+    },
+    "accessOrMutate": function (_) {
+        return (
+            _.shape !== "iterator" &&
+            !_.unique &&
+            !_.sorted &&
+            _.long
+        ) ? "show" : "hide";
+    },
+    "access": function (_) {
+        return this.accessOrMutate(_);
+    },
+    "mutate": function (_) {
+        return this.accessOrMutate(_);
+    },
+    "churn": function (_) {
+        return (
+            _.shape === "flat" &&
+            !_.sorted &&
+            !_.unique &&
+            !_.capacity &&
+            _.long &&
+            _.shift &&
+            !_.splice &&
+            !_.index
+        ) ? "show" : "hide";
     },
 
     // Whether available and recommended
     "list": function (_) {
-        return _.shape === "non-map";
+        return (
+            _.shape === "flat" &&
+            !_.sorted &&
+            !_.unique &&
+            !_.capacity &&
+            !(_.long && _.index)
+        ) ? (
+            (
+                _.long && (
+                    _.splice ||
+                    (_.shift && _.churn === "garbage")
+                )
+            ) ? "recommended" : "available"
+        ) : "unavailable";
     },
     "deque": function (_) {
-        return _.shape === "non-map";
+        return (
+            _.shape === "flat" &&
+            !_.sorted &&
+            !_.unique &&
+            !_.capacity &&
+            !(_.long && _.splice)
+        ) ? (
+            (
+                _.long && _.shift && (
+                    _.index ||
+                    _.churn === "shift"
+                )
+            ) ? "recommended" : "available"
+        ) : "unavailable";
     },
     "map": function (_) {
         return (
-            _shape === "map" &&
+            _.shape === "map" &&
             !_.sorted &&
             !_.capacity &&
-            !_.multi
-        );
+            !_.multi &&
+            !(_.keys === "objects" && _.weak)
+        ) ? (
+            (
+                _.ordered &&
+                (_.keys === "any" || _.keys === "objects")
+            ) ? "recommended" : "available"
+        ) : "unavailable";
     },
     "set": function (_) {
-        return _.shape === "non-map";
+        return (
+            _.shape === "flat" &&
+            _.unique &&
+            !_.sorted &&
+            !_.capacity
+        ) ? (
+            _.ordered ? "recommended" : "available"
+        ) : "unavailable";
     },
     "heap": function (_) {
-        return _.shape === "non-map";
+        return (
+            _.shape === "flat" &&
+            _.max // TODO
+        ) ? "available" : "unavailable";
     },
     "dict": function (_) {
         return (
@@ -82,24 +199,59 @@ exports.answers = {
             !_.capacity &&
             !_.multi &&
             _.keys === "strings"
-        ) ? "available" : "unavailable";
+        ) ? "recommended" : "unavailable";
     },
     "sorted-array": function (_) {
         return (
-            _.shape === "non-map"
-        );
+            _.shape === "flat" &&
+            _.sorted &&
+            !_.unique
+        ) ? "available" : "unavailable";
+    },
+    "sorted-set": function (_) {
+        return (
+            _.shape === "flat" &&
+            _.unique &&
+            _.sorted
+        ) ? (
+            _.long ? "recommended" : "available"
+        ) : "unavailable";
     },
     "fast-set": function (_) {
-        return _.shape === "non-map";
+        return (
+            _.shape === "flat" &&
+            _.unique &&
+            !_.capacity &&
+            !_.ordered &&
+            !_.sorted
+        ) ? "recommended" : "unavailable";
     },
     "lfu-set": function (_) {
-        return _.shape === "non-map";
+        return (
+            _.shape === "flat" &&
+            _.unique &&
+            _.capacity
+        ) ? (
+            _.strategy === "frequency" ? "recommended" : "available"
+        ) : "unavailable";
     },
     "lru-set": function (_) {
-        return _.shape === "non-map";
+        return (
+            _.shape === "flat" &&
+            _.unique &&
+            _.capacity
+        ) ? (
+            _.strategy === "recency" ? "recommended" : "available"
+        ) : "unavailable";
     },
     "sorted-array-set": function (_) {
-        return _.shape === "non-map";
+        return (
+            _.shape === "flat" &&
+            _.unique &&
+            _.sorted
+        ) ? (
+            !_.long ? "recommended" : "available"
+        ) : "unavailable";
     },
     "fast-map": function (_) {
         return (
@@ -107,31 +259,53 @@ exports.answers = {
             !_.sorted &&
             !_.capacity &&
             !_.multi &&
-            !_.ordered
-        ) ? "available" : "unavailable";
+            !_.ordered &&
+            !(_.keys === "objects" && _.weak)
+        ) ? (
+            _.keys === "any" ? "recommended" : "available"
+        ) : "unavailable";
     },
     "lfu-map": function (_) {
         return (
             _.shape === "map" &&
-            _.capacity &&
-            _.strategy === "frequency"
-        );
+            _.capacity
+        ) ? (
+            _.strategy === "frequency" ? "recommended" : "available"
+        ) : "unavailable";
     },
     "lru-map": function (_) {
         return (
             _.shape === "map" &&
-            _.capacity &&
-            _.strategy === "recency"
-        );
+            _.capacity
+        ) ? (
+            _.strategy === "recency" ? "recommended" : "available"
+        ) : "unavailable";
     },
     "multi-map": function (_) {
-        return _.shape === "map";
+        return (
+            _.shape === "map" &&
+            _.multi
+        ) ? "recommended" : "unavailable";
     },
     "sorted-map": function (_) {
-        return _.shape === "map";
+        return (
+            _.shape === "map" &&
+            !_.multi &&
+            !_.capacity &&
+            _.sorted
+        ) ? (
+            _.long ? "recommended" : "available"
+        ) : "unavailable";
     },
     "sorted-array-map": function (_) {
-        return _.shape === "map";
+        return (
+            _.shape === "map" &&
+            _.sorted &&
+            !_.multi &&
+            !_.capacity
+        ) ? (
+            !_.long ? "recommended" : "available"
+        ) : "unavailable";
     },
     "weak-map": function (_) {
         return (
@@ -140,27 +314,45 @@ exports.answers = {
             !_.capacity &&
             !_.multi &&
             !_.ordered &&
-            !_.keys === "objects"
-        ) ? "available" : "unavailable";
+            _.keys === "objects"
+        ) ? "recommended" : "unavailable";
     },
     "iterator": function (_) {
-        return _.shape === "iterator";
+        return (
+            _.shape === "iterator"
+        ) ? "recommended" : "unavailable";
     },
     "array": function (_) {
         return (
-            (
-                _.shape === "map" &&
-                !_.sorted &&
-                !_.capacity &&
-                !_.multi &&
-                _.keys === "numbers"
-            ) || (
-                _.shape === "non-map"
+            !_.sorted &&
+            !_.capacity &&
+            !_.unique &&
+            !_.multi && (
+                (
+                    _.shape === "map" &&
+                    _.keys === "numbers"
+                ) || (
+                    _.shape === "flat" &&
+                    !(_.long && _.shift)
+                )
             )
-        );
+        ) ? (
+            (
+                _.shape === "map" ||
+                (
+                    _.shape === "flat" &&
+                    (
+                        !(_.long && _.splice) ||
+                        (_.long && _.index && _.splice)
+                    )
+                )
+            ) ? "recommended" : "available"
+        ) : "unavailable";
     },
     "object": function (_) {
-        return false;
+        return (
+            _.shape === "map" && false // TODO
+        ) ? "available" : "unavailable";
     }
 
 };
--- _generate/lib/index.js
+++ _generate/lib/index.js
@@ -1,1 +1,98 @@
 
+var Query = require("./choose").Query;
+var questions = require("./choose").questions;
+var Dict = require("collections/dict");
+
+var existingClasses = {
+    shape: "hide",
+    keys: "hide",
+    capacity: "hide",
+    strategy: "hide",
+    weak: "hide",
+    unique: "hide",
+    ordered: "hide",
+    sorted: "hide",
+    multi: "hide",
+    "long": "hide",
+    churn: "hide",
+    access: "hide",
+    mutate: "hide",
+    list: "unavailable",
+    object: "unavailable",
+    map: "unavailable",
+    "fast-map": "unavailable",
+    "lru-map": "unavailable",
+    "lfu-map": "unavailable",
+    "sorted-map": "unavailable",
+    "sorted-array-map": "unavailable",
+    "dict": "unavailable",
+    "multi-map": "unavailable",
+    "weak-map": "unavailable",
+    "deque": "unavailable",
+    "array": "unavailable",
+    "set": "unavailable",
+    "fast-set": "unavailable",
+    "lru-set": "unavailable",
+    "lfu-set": "unavailable",
+    "sorted-set": "unavailable",
+    "sorted-array-set": "unavailable",
+    "sorted-array": "unavailable",
+    "heap": "unavailable",
+    "iterator": "unavailable"
+};
+var form = document.getElementById("choose");
+
+[
+    form.shape[0],
+    form.shape[1],
+    form.shape[2],
+    form.keys[0],
+    form.keys[1],
+    form.keys[2],
+    form.keys[3],
+    form.sorted,
+    form.weak,
+    form.unique,
+    form.capacity,
+    form.strategy[0],
+    form.strategy[1],
+    form.ordered,
+    form.multi,
+    form.index,
+    form.shift,
+    form.splice,
+    form.long,
+    form.churn[0],
+    form.churn[1]
+].forEach(function (element) {
+    element.addEventListener("change", handleChange);
+});
+
+function handleChange() {
+    var query = new Query();
+
+    query.shape = form.shape.value;
+    query.keys = form.keys.value;
+    query.unique = form.unique.checked;
+    query.weak = form.weak.checked;
+    query.capacity = (query.shape === "map" || query.unique) && form.capacity.checked;
+    query.strategy = form.strategy.value;
+    query.ordered = !query.weak && form.ordered.checked;
+    query.sorted = query.ordered && form.sorted.checked;
+    query.multi = form.multi.checked;
+    query.index = form.index.checked;
+    query.splice = form.splice.checked;
+    query.long = form.long.checked;
+    query.churn = form.churn.value;
+
+    for (var id in questions) {
+        var element = document.getElementById(id);
+        if (!element) continue; // XXX
+        element.classList.remove(existingClasses[id]);
+        var className = questions[id](query);
+        existingClasses[id] = className;
+        element.classList.add(className);
+    }
+
+}
+
